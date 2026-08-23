e7177c6f9dae596756ac3e389777857a211db982
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-08-23 14:10:48 +0530

    Added student information program

---
+++ StudentInfo.java
@@ -0,0 +1,13 @@
+public class StudentInfo {
+
+    public static void main(String[] args) {
+     
+         System.out.println("My name is Parbej.");
+         System.out.println("I am learning Java");
+         System.out.println("I am a student");
+         System.out.println("I want to become a good programmer");
+
+}
+
+}
+
