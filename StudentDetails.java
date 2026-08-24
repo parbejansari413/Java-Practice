eee3950f1e3678a61b03f22f5a62ec7456c38700
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-08-24 17:12:01 +0530

    Java program for variables

---
+++ StudentDetails.java
@@ -0,0 +1,18 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        //Student variables 
+        String name = "Parbej";
+        int age = 23;
+        double cgpa = 8.10;
+        char grade = 'A';
+
+        // Printing the variables
+        System.out.println("Student Details");
+        System.out.println("Name: " + name );
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade:" + grade);         
+    }
+
+}
