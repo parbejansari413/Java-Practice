a782c15eab820893881cccdc95c4c310b273795a
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-07 16:45:37 +0530

    saved  LoopEven code

---
+++ Loop2.java
@@ -0,0 +1,7 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        for(int i = 500; i >= 50; i -= 10) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ LoopEven.java
@@ -0,0 +1,8 @@
+public class LoopEven {
+    public static void main(String[] args) {
+        for(int i = 4; i <= 30; i+=4) {
+            System.out.println(i);
+
+        }
+    }
+}
