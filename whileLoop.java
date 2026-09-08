b5d47fb2b570cef1c6c5fad6ca2d8c4cb40bf905
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-08 15:04:00 +0530

    push while1 java

--- whileLoop.java
+++ whileLoop.java
@@ -1,9 +1,9 @@
 public class whileLoop {
     public static void main(String[] args) {
-        int i = 1;
-        while(i <= 10) {
+        int i = 10;
+        while(i >= 1) {
             System.out.println(i);
-            i++;
+            i--;
         }
     }
 }
