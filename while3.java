14d84713b7bfa5a164c0508713b72d2356e6a6c3
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-08 16:31:39 +0530

    push while5 code

---
+++ while3.java
@@ -0,0 +1,11 @@
+public class while3 {
+    public static void main(String[] args) {
+        
+    
+    int i = 5;
+    while(i <= 50) {
+        System.out.println(i);
+        i+=5;
+    }
+}
+}
---
+++ while4.java
@@ -0,0 +1,12 @@
+public class while4 {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while( i <= 100){
+           sum = sum + i;
+           i++;
+
+        }
+        System.out.println("Sum = " + sum);
+}
+}
