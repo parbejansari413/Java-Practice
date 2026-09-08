d6fd1d22eb8289f36f1e9dadd1bd349c48db1b98
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-08 15:07:13 +0530

    add while2 java

---
+++ while2.java
@@ -0,0 +1,12 @@
+public class while2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 30) {
+            if(i % 2 != 0) {
+                System.out.println(i);
+                
+            }
+            i++;
+        }
+    }
+}
