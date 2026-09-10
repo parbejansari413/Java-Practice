1795a09e49312ca46a79995390433275431ac240
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-10 14:06:12 +0530

    add Array4 code

---
+++ Array4.java
@@ -0,0 +1,12 @@
+public class Array4 {
+    public static void main(String[] args) {
+        int [] numbers = {20,30,40,50,60};
+        numbers[3] = 90;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
