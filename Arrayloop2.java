4cef3f8cde63eb7036859d16e132fa45db98de53
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-10 15:24:41 +0530

    Arrayloop2

---
+++ Arrayloop2.java
@@ -0,0 +1,9 @@
+public class Arrayloop2 {
+    public static void main(String[] args) {
+        int[] numbers = {40,50,60,70,80,90};
+
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println("Index: " + i + " : " + numbers[i]);
+        }
+    }
+}
