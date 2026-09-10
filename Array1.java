d1c3187d2982ac54e16477199057b056905d481d
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-10 13:51:43 +0530

    Shift Array1 code

---
+++ Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] numbers = {50,60,70,80,90};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+
+    }
+}
