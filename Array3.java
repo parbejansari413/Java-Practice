fc3178e1743c99e4c344a8f84c35226740384954
parbejansari413 <parbejansari.2301066@srec.ac.in>
2026-09-10 13:59:20 +0530

    push Array3 code

---
+++ Array3.java
@@ -0,0 +1,7 @@
+public class Array3 {
+    public static void main(String[] args) {
+        int[] numbers = {30,40,50,60,70,80,90};
+
+        System.out.println("Array length: " + numbers.length);
+    }
+}
